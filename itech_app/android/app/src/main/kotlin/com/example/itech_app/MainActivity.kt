e0c37d19736f14ff3509ede7fcc31d00b6f5f74b
justin nabunturan <justinnabunturan1234567890@gmail.com>
2026-07-30 09:22:35 +0800

    Delete itech_app directory

--- itech_app/android/app/src/main/kotlin/com/example/itech_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.itech_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- itech_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'app/pup_iitech_app.dart';
-import 'screens/role_selection/role_selection_screen.dart';
-
-void main() {
-  runApp(const PupItechApp(home: RoleSelectionScreen()));
-}
-
